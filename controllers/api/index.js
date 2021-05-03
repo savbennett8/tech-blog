493474dcdfa46a492daeed4c74198a06a3dc2f98
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-02 19:51:06 -0600

    fixed typo and starting to seed files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
-const commentRoutes = require('./comments-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
