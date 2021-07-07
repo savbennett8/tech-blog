8157f3199928d89cbc6dece6d47e123bc4b7a8a1
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-06 20:43:18 -0600

    small changes to login/out & signup functionality

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -7,7 +7,7 @@ async function signupFormHandler(event) {
 
     if (username && email && password) {
         const response = await fetch('/api/users', {
-            method: 'post',
+            method: 'POST',
             body: JSON.stringify({
                 username,
                 email,
@@ -17,8 +17,8 @@ async function signupFormHandler(event) {
         });
         //check the response status
         if (response.ok) {
-            console.log('success');
-            document.location.replace('/');
+            window.alert('New user was created; please sign in!');
+            document.location.reload();
         } else {
             alert(response.statusText);
         }
