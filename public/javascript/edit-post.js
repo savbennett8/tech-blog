f8f22c16803e3cc72edcc5889ab9bd51b066474c
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-06 18:11:34 -0600

    created functions to create / edit / delete posts

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,27 @@
+async function editPost(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const post_body = document.querySelector('input[name="post-body"]').value.trim();
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            post_body
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert('There was an error!' + response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editPost);
